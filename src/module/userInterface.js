43ef901a18e4719044a0fa52f9e3e6255de6302c
Rouni Abdo <ra223aa@student.lnu.se>
2024-09-27 14:31:59 +0200

    Add method to add an item to the VAT calculator

--- src/module/userInterface.js
+++ src/module/userInterface.js
@@ -11,4 +11,20 @@ export class UserInterface {
   constructor (VatRates) {
     this.vatCalculator = new VatCalculator(VatRates)
   }
+
+  /**
+   * Add an item to the VAT calculator.
+   *
+   * @param {string} name - Name of the item.
+   * @param {number} price - Price of the item.
+   * @param {string} vatRateName - VAT rate name.
+   * @param {number} quantity - Quantity of the item.
+   */
+  addItemToCalculator (name, price, vatRateName, quantity = 1) {
+    try {
+      this.vatCalculator.addItem(name, price, vatRateName, quantity)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
 }
