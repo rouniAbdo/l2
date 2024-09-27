0ee252041339ba4698dea35a019bba086e898104
Rouni Abdo <ra223aa@student.lnu.se>
2024-09-27 15:13:17 +0200

    Add method to return success & errors message

--- src/module/userInterface.js
+++ src/module/userInterface.js
@@ -19,12 +19,37 @@ export class UserInterface {
    * @param {number} price - Price of the item.
    * @param {string} vatRateName - VAT rate name.
    * @param {number} quantity - Quantity of the item.
+   * @returns {string} - Success message.
    */
   addItemToCalculator (name, price, vatRateName, quantity = 1) {
     try {
       this.vatCalculator.addItem(name, price, vatRateName, quantity)
+      return this.successNotification(name, price, vatRateName, quantity)
     } catch (error) {
-      throw new Error(error)
+      return this.errorMessage(error)
     }
+  }
+
+  /**
+   * Generate a success message.
+   *
+   * @param {string} name - Name of the item.
+   * @param {number} price - Price of the item.
+   * @param {number} vatRateName - VAT rate name.
+   * @param {number} quantity - Quantity of the item.
+   * @returns {string} - Success message.
+   */
+  successNotification (name, price, vatRateName, quantity) {
+    return `Added ${quantity} units of ${name} at price ${price} each with VAT rate ${vatRateName}`
+  }
+
+  /**
+   * Generate an error message.
+   *
+   * @param {Error} error - Error object.
+   * @returns {string} - Error message.
+   */
+  errorMessage (error) {
+    return error.message
   }
 }
